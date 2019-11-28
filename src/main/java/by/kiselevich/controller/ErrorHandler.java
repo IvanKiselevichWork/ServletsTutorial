1c9bc8a31d15706de21a100b7585602563ca68bc
ivankiselevich <gref2259@gmail.com>
2019-11-28 09:28:47 +0300

    add errorhandler servlet and error handling in web.xml

---
+++ src/main/java/by/kiselevich/controller/ErrorHandler.java
@@ -0,0 +1,68 @@
+package by.kiselevich.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ErrorHandler", urlPatterns = {"/errorhandler"})
+public class ErrorHandler extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Analyze the servlet exception
+        Throwable throwable = (Throwable) req.getAttribute("javax.servlet.error.exception");
+        Integer statusCode = (Integer) req.getAttribute("javax.servlet.error.status_code");
+        String servletName = (String) req.getAttribute("javax.servlet.error.servlet_name");
+
+        if (servletName == null) {
+            servletName = "Unknown";
+        }
+        String requestUri = (String) req.getAttribute("javax.servlet.error.request_uri");
+
+        if (requestUri == null) {
+            requestUri = "Unknown";
+        }
+
+        // Set response content type
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        String title = "Error/Exception Information";
+        String docType =
+                "<!doctype html public \"-//w3c//dtd html 4.0 " +
+                        "transitional//en\">\n";
+
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor = \"#f0f0f0\">\n");
+
+        if (throwable == null && statusCode == null) {
+            out.println("<h2>Error information is missing</h2>");
+        } else {
+            out.println("<h2>Error information</h2>");
+            if (statusCode != null) {
+                out.println("The status code : " + statusCode + "</br></br>");
+            }
+            if (throwable != null) {
+                out.println("Exception Type : " + throwable.getClass( ).getName( ) + "</br></br>");
+                out.println("The exception message: " + throwable.getMessage( ));
+            }
+        }
+        out.println("Servlet Name : " + servletName + "</br></br>");
+        out.println("The request URI: " + requestUri + "<br><br>");
+        out.println("Please return to the <a href=\"" +
+                resp.encodeURL("http://localhost:8080/ServletsTutorial_war_exploded/") +
+                "\">Home Page</a>.");
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
